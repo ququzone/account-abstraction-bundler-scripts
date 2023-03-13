f9a64ccec1e8ce94eb32aad893a6fb9b2499551d
ququzone <xueping.yang@gmail.com>
2023-03-13 16:06:39 +0800

    add get entrypoints rpc

--- src/entrypoint.ts
+++ src/entrypoint.ts
@@ -1,17 +1,10 @@
-// import { ethers, providers } from "ethers"
-// import { SimpleAccountFactory__factory } from "../../../typechain"
+import { providers } from 'ethers'
 
 async function main() {
-  // const provider = new providers.JsonRpcProvider(`http://localhost:4337`)
-  // const factory = SimpleAccountFactory__factory.connect("tokenAddress", provider)
-  // const nonce = 0
-  // const account = await factory.getAddress(owner.address, nonce)
-  // const code = await ethers.provider.getCode(account)
-  // if (code !== "0x") {
-  //     console.log(`account ${account} already created`)
-  //     return
-  // }
-  // console.log(`entrypoints address: ${entrypoints}`)
+  const provider = new providers.JsonRpcProvider(`http://localhost:4337`)
+  const entrypoints = await provider.send('eth_supportedEntryPoints', [])
+
+  console.log(`entrypoints address: ${entrypoints}`)
 }
 
 main()
